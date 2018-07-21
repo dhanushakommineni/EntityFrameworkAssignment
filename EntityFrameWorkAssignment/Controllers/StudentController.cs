86eb95774e1f528f862aae29e52053a5bdb35cd3
CORP\Dhanusha.Kommineni <Dhanusha.Kommineni@neudesic.com>
2018-07-21 19:33:40 +0530

    assignment

---
+++ EntityFrameWorkAssignment/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameWorkAssignment.DataContext;
+using EntityFrameWorkAssignment.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameWorkAssignment.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Student")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (var StudentContext = new StudentData())
+            {
+                var StudentsList = new List<StudentModel>();
+                StudentsList = StudentContext.StudentsDetails.ToList();
+                return Ok(StudentsList);
+            }
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody]StudentModel StudentObject)
+        {
+            using (var StudentContext = new StudentData())
+            {
+                StudentContext.StudentsDetails.Add(StudentObject);
+                StudentContext.SaveChanges();
+            }
+            return Ok("StudentAdded");
+        }
+        [HttpPut]
+        public ActionResult Put([FromBody]StudentModel StudentObject)
+        {
+            using (var StudentContext = new StudentData())
+            {
+                StudentContext.StudentsDetails.Update(StudentObject);
+                StudentContext.SaveChanges();
+            }
+            return Ok("StudentUpdated");
+        }
+        [HttpDelete]
+        public ActionResult Delete([FromBody]StudentModel StudentObject)
+        {
+            using (var StudentContext = new StudentData())
+            {
+                StudentContext.StudentsDetails.Remove(StudentObject);
+                StudentContext.SaveChanges();
+            }
+            return Ok("StudentDeleted");
+        }
+    }
+}
---
+++ EntityFrameWorkAssignment/DataContext/StudentData.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameWorkAssignment.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace EntityFrameWorkAssignment.DataContext
+{
+    public class StudentData : DbContext
+    {
+        public DbSet<StudentModel> StudentsDetails { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; Database=Student; Trusted_Connection=True; MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
